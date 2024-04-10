2e75f1ecdd1c6f37e6548fbdd05363f6ac0fb22a
najmuddin_rashid <rashidnajmuddin748@gmail.com>
2024-04-10 14:34:31 +0530

    new Outlet app

---
+++ lib/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text(
+          'Welcome to Home Screen!',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
